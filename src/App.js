7bea3bf5f14448507d5f664a4c97f69d732a4eed
Challa Sai Krishna Reddy <challasaikrishna31@gmail.com>
2022-07-08 10:54:16 +0530

    Modified Settings Display Name

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => {
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            <TooltipComponent content="Settings" position="Top">
+            <TooltipComponent content="Theme Settings" position="Top">
               <button
                 type="button"
                 onClick={() => setThemeSettings(true)}
